6af6333c1f75ba44e33e3636986d1e839e18d9f2
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-01-20 20:39:47 -0300

    feat(HabitDay): added date, amount and completed props && change style based in progress

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,4 +1,8 @@
 import { TouchableOpacity, Dimensions, TouchableOpacityProps } from "react-native";
+import clsx from "clsx";
+import dayjs from "dayjs";
+
+import { generateProgressPercentage } from "../utils/generate-progress-percentage";
 
 const WEEK_DAYS = 7;
 const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
@@ -6,12 +10,31 @@ const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
 export const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_HORIZONTAL_PADDING + 5);
 
-interface Props extends TouchableOpacityProps {}
+interface Props extends TouchableOpacityProps {
+	date: Date;
+	amount?: number;
+	completed?: number;
+}
 
-export const HabitDay = ({ ...rest }: Props) => {
+export const HabitDay = ({ date, amount = 0, completed = 0, ...rest }: Props) => {
+	const completedPercentage = amount > 0
+		? generateProgressPercentage(amount, completed)
+		: 0;
+
+	const today = dayjs().startOf('day').toDate();
+	const isCurrentDay = dayjs(date).isSame(today);
+
 	return (
 		<TouchableOpacity
-			className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-900"
+			className={clsx("rounded-lg border-2 m-1", {
+				'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+				'bg-violet-900 border-violet-500': completedPercentage > 0 && completedPercentage < 20,
+				'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
+				'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
+				'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
+				'bg-violet-500 border-violet-400': completedPercentage >= 80,
+				'border-white border-4': isCurrentDay
+			})}
 			activeOpacity={0.7}
 			style={{
 				width: DAY_SIZE,
